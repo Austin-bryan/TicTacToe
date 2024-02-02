c15ae0e615c5d25cba2da8b67bfdbd9a71aee513
Austin Bryan <w.austin.bryan@gmail.com>
2024-02-02 12:44:09 -0500

    AddedTurnManager

---
+++ TicTacToe/TurnManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe;
+
+public static class TurnManager
+{
+    private static List<Player> Players;
+    public static Player ActivePlayer;
+    private static int playerIndex = 1;
+
+    static TurnManager()
+    {
+        Players = new List<Player>
+        {
+            new('X', Color.Red),
+            new('O', Color.Blue)
+        };
+        ActivePlayer = Players[0];
+    }
+    public static void NextTurn() => ActivePlayer = Players[playerIndex++ % 2];
+}
